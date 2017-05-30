6e48070721814eee4af02ace8f1894f559a215aa
Lhudhovik <ludovic.geraert@gmail.com>
2017-05-30 21:31:26 +0200

    + atlases handling

--- resources/assets/AssetManager.hx
+++ resources/assets/AssetManager.hx
@@ -11,6 +11,8 @@ import mloader.XmlLoader;
 import msignal.Signal.Signal0;
 import msignal.Signal.Signal1;
 import openfl.Assets;
+import openfl.display.BitmapData;
+import starling.textures.Texture;
 import starling.textures.TextureAtlas;
 
 
@@ -36,7 +38,7 @@ class AssetManager
 	private var onCancel:Signal0;
 	private var onStart:Signal0;
 	private var onError:Signal1<LoaderErrorType>;
-	
+	private var requestedAtlasQueue:Array<String>;
 	
 	public function new() 
 	{
@@ -59,6 +61,8 @@ class AssetManager
 		loaders = new Map<String, Loader<Dynamic>>();
 		atlases = new Map<String, TextureAtlas>();
 		
+		requestedAtlasQueue = new Array<String>();
+		
 		onComplete = new Signal0();
 		onProgress = new Signal1(Float);
 		onStart = new Signal0();
@@ -67,15 +71,15 @@ class AssetManager
 		
 		loaderQueue = new LoaderQueue();
 		loaderQueue.ignoreFailures = false;
-		loaderQueue.loaded.add(onLoadingEvent);
+		loaderQueue.loaded.add(OnLoadingEvent);
 		
 		
 	}
 	
 	
-	 public function append(type:AssetType, url:String, loaderName:String, onCompleteCallback:LoaderEvent<Dynamic>->Void = null, onProgressCallback:LoaderEvent<Dynamic>->Void = null, onErrorCallback:LoaderEvent<Dynamic>->Void = null, onCancelCallback:LoaderEvent<Dynamic>->Void = null):Void
+	 public function Append(type:AssetType, url:String, loaderName:String, onCompleteCallback:LoaderEvent<Dynamic>->Void = null, onProgressCallback:LoaderEvent<Dynamic>->Void = null, onErrorCallback:LoaderEvent<Dynamic>->Void = null, onCancelCallback:LoaderEvent<Dynamic>->Void = null):Void
 	{
-		
+	
 		if (loaderName == null)
 			loaderName = DEFAULT_LOADER_NAME+ Date.now().getTime();
 		
@@ -101,10 +105,18 @@ class AssetManager
 				loaders[loaderName] = new StringLoader(url);
 				
 			case AssetType.SOUND :
-				//loaders[loaderName] = new String(url);
+				loaders[loaderName] = new StringLoader(url);
 				
-			
+			case AssetType.ATLAS:
+				if (requestedAtlasQueue.indexOf(loaderName) == -1){
+					requestedAtlasQueue.push(loaderName);
+					Append(AssetType.XML, '${url}/${loaderName}.xml', '${loaderName}_xml', onCompleteCallback, onProgressCallback, onErrorCallback, onCancelCallback);
+					Append(AssetType.IMAGE, '${url}/${loaderName}.png', '${loaderName}_image', onCompleteCallback, onProgressCallback, onErrorCallback, onCancelCallback);
+					
+				}
+				return;
 		}
+		
 		
 		if (onCompleteCallback != null)
 			loaders[loaderName].loaded.addOnce(onCompleteCallback).forType(LoaderEventType.Complete);
@@ -114,14 +126,15 @@ class AssetManager
 			//loaders[loaderName].loaded.addOnce(onErrorCallback).forType(LoaderEventType.Fail(LoaderErrorType.Data));
 		if (onCancelCallback!=null)
 			loaders[loaderName].loaded.addOnce(onCancelCallback).forType(LoaderEventType.Cancel);
-		
+		trace(loaderName);
+		trace(url);
 		
 		loaderQueue.add(loaders[loaderName]);
 		
 		
 	}
 	
-	public function removeLoader(name:String = ""):Void
+	public function RemoveLoader(name:String = ""):Void
 	{
 		loaders[name].cancel();
 		loaders[name] = null;
@@ -129,7 +142,7 @@ class AssetManager
 		
 	}
 	
-	public function load(onCompleteCallback:Void->Void = null, onProgressCallback:Float->Void = null, onErrorCallback:LoaderErrorType->Void = null, onCancelCallback:Void->Void = null):Void
+	public function Load(onCompleteCallback:Void->Void = null, onProgressCallback:Float->Void = null, onErrorCallback:LoaderErrorType->Void = null, onCancelCallback:Void->Void = null):Void
 	{
 		
 		if (onCompleteCallback != null)
@@ -140,12 +153,11 @@ class AssetManager
 			onError.addOnce(onErrorCallback);
 		if (onCancelCallback!=null)
 			onCancel.addOnce(onCancelCallback);
-		trace("jusque là...");
 		loaderQueue.load();
 	
 	}
 	
-	private function onLoadingEvent(e:LoaderEvent<Dynamic> = null):Void
+	private function OnLoadingEvent(e:LoaderEvent<Dynamic> = null):Void
 	{
 		
 		switch(e.type){
@@ -153,13 +165,23 @@ class AssetManager
 			case LoaderEventType.Start:
 				onStart.dispatch();
 			case LoaderEventType.Complete:
+				trace(requestedAtlasQueue);
+				for (requestedAtlas in requestedAtlasQueue){
+					
+					CreateAtlas(requestedAtlas);
+					requestedAtlasQueue.remove(requestedAtlas);
+				}
+				
+				trace(requestedAtlasQueue);
 				onComplete.dispatch();
 			case LoaderEventType.Progress:
 				onProgress.dispatch(get_progress());
 			case LoaderEventType.Cancel:
+				while(requestedAtlasQueue.length>0) requestedAtlasQueue.pop();
 				onCancel.dispatch();	
 				//Errors
 			case LoaderEventType.Fail(e):
+				while(requestedAtlasQueue.length>0) requestedAtlasQueue.pop();
 				onError.dispatch(e);
 		}
 		
@@ -180,7 +202,34 @@ class AssetManager
 		return content;
 	}
 	
+	
+	private function CreateAtlas(atlasName:String):Void{
 		
+		if (atlases[atlasName] == null){
+			trace(Type.getClassName(getContent('${atlasName}_image')));
+			//var texture : Texture = Texture.fromBitmapData(getContent('${atlasName}_image'));
+			//atlases[atlasName] = new TextureAtlas(Texture.fromBitmapData(cast(getContent('${atlasName}_image'), BitmapData)), cast(getContent('${atlasName}_xml'), Xml));
+		}else throw "Atlas already exists";
+		
+	}
+	
+	public function GetAtlas(atlasName:String):TextureAtlas
+	{
+		return atlases[atlasName] != null ? atlases[atlasName] : null;
+	}
+	public function GetTexture(textureName:String, atlasName:String):TextureAtlas
+	{
+		return atlases[atlasName] != null ? atlases[atlasName] : null;
+	}
+	public function ClearAtlas(atlasName:String):Void
+	{
+		if ( atlases[atlasName] != null ){
+			atlases[atlasName].dispose();
+			atlases[atlasName] = null;
+			
+		}
+	}
+	
 	public function cancel():Void
 	{
 		loaderQueue.cancel();
@@ -193,4 +242,5 @@ enum AssetType{
 		XML;
 		DATA;
 		SOUND;
+		ATLAS;
 	}
