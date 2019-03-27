839d5d44433e79b0d51c16c684604bc67d5fff16
Lhudhovik <ludovic.geraert@gmail.com>
2019-03-27 22:14:20 +0100

    *work in progress

--- beardFramework/graphics/core/Visual.hx
+++ beardFramework/graphics/core/Visual.hx
@@ -4,9 +4,11 @@ import beardFramework.graphics.cameras.Camera;
 import beardFramework.graphics.rendering.Renderer;
 import beardFramework.graphics.rendering.RenderingData;
 import beardFramework.graphics.rendering.Shaders;
+import beardFramework.graphics.rendering.lights.LightManager;
 import beardFramework.graphics.rendering.vertexData.RenderedDataBufferArray;
 import beardFramework.graphics.rendering.vertexData.VertexAttribute;
 import beardFramework.interfaces.IRenderable;
+import beardFramework.utils.graphics.GLU;
 import haxe.ds.Vector;
 import lime.graphics.opengl.GL;
 import lime.graphics.opengl.GLBuffer;
@@ -22,9 +24,11 @@ import lime.utils.UInt16Array;
  */
 class Visual extends AbstractVisual implements IRenderable
 {
-	private static var vertices:Vector<Float>; //overide with local variable if necessary
+	private static var TEMPLATE(default, never):String ="visual" ; //overide with local variable if necessary
+	private static var verticesData:Float32Array; //overide with local variable if necessary
 	private static var indices:UInt16Array;
-	private static var VBOs:Map<String, GLBuffer>;
+	private static var VBO:GLBuffer;
+	private static var EBO:GLBuffer;
 	
 	@:isVar public var readyForRendering(get, null):Bool;
 	
@@ -35,10 +39,8 @@ class Visual extends AbstractVisual implements IRenderable
 	
 	
 	private var vertexAttributes:Vector<VertexAttribute>;
-	private var EBO:GLBuffer;
-	private var VBO:GLBuffer;
 	private var VAO:GLVertexArrayObject;
-	private var verticesData:Float32Array;
+	//private var verticesData:Float32Array;
 	private var renderer:Renderer;
 	
 	
@@ -46,14 +48,7 @@ class Visual extends AbstractVisual implements IRenderable
 	public function new(texture:String, atlas:String, name:String="") 
 	{
 		super(texture, atlas, name);
-		if (vertices == null) vertices = Vector.fromArrayCopy([
-		0, 1, 0.0, 1.0,
-		1, 1, 1.0, 1.0,
-		1, 0, 1.0, 0.0,
-		0, 0, 0.0, 0.0]);
-		
-		if (indices == null) indices = new UInt16Array([0, 1, 2, 2, 3, 0]);
-		
+				
 		cameras = new List();
 		for (camera in BeardGame.Get().cameras)
 			cameras.add(camera.name);
@@ -65,11 +60,22 @@ class Visual extends AbstractVisual implements IRenderable
 		
 		renderer = Renderer.Get();
 		drawMode = data.drawMode;
-		verticesData = new Float32Array(data.vertexStride*4);
+	
 		lightGroup = data.lightGroup;
 		
 		InitShaders(data.shaders);
-		InitBuffers(data.vertexAttributes, data.vertexStride);
+		if (verticesData == null){
+			if (vertices == null) vertices = Vector.fromArrayCopy([
+		0, 1, 0.0, 1.0,
+		1, 1, 1.0, 1.0,
+		1, 0, 1.0, 0.0,
+		0, 0, 0.0, 0.0]);
+		
+		if (indices == null) indices = new UInt16Array([0, 1, 2, 2, 3, 0]);
+			
+			
+			InitBuffers(renderer.GetTemplate(TEMPLATE).vertexAttributes, renderer.GetTemplate(TEMPLATE).vertexStride);
+		}
 		
 		cameras = new List();
 		
@@ -78,7 +84,7 @@ class Visual extends AbstractVisual implements IRenderable
 	}
 	
 		
-	public function InitShaders(shadersList:Array<Shaders.Shader>):Void
+	public function InitShaders(shadersList:Array<Shader>):Void
 	{
 		var createdShaders:Array<GLShader> = [];
 		for (shader in shadersList)
@@ -151,7 +157,7 @@ class Visual extends AbstractVisual implements IRenderable
 		}
 			
 		
-		if (indicesData.length > 0)
+		if (indices.length > 0)
 		{
 			EBO  = renderer.GenerateBuffer();
 		
@@ -172,7 +178,7 @@ class Visual extends AbstractVisual implements IRenderable
 		return readyForRendering;
 	}
 	
-	private function SetUniforms():Void
+	private inline function SetUniforms():Void
 	{
 		
 		GL.useProgram(shaderProgram);
@@ -192,25 +198,29 @@ class Visual extends AbstractVisual implements IRenderable
 		}
 		
 		//GL.bindVertexArray(VAO);
-		GL.bindBuffer(GL.ARRAY_BUFFER, VBO);
-		var pointer:Int;
-		var stride:Int = 0;
-		for (attribute in vertexAttributes)
-		{
+		if (renderer.boundBuffer != VBO){
+			GL.bindBuffer(GL.ARRAY_BUFFER, VBO);
+			renderer.boundBuffer = VBO;
+			var pointer:Int;
+			var stride:Int = 0;
+			for (attribute in vertexAttributes)
+			{
+				
+				//trace(attribute);
+				pointer = GL.getAttribLocation(shaderProgram, attribute.name);
+				GL.enableVertexAttribArray(pointer);
+				//GL.enableVertexAttribArray(attribute.index);
+				//GL.vertexAttribPointer(attribute.index, attribute.size, GL.FLOAT, false, renderedData.vertexStride * Float32Array.BYTES_PER_ELEMENT, stride* Float32Array.BYTES_PER_ELEMENT);
+				GL.vertexAttribPointer(pointer, attribute.size, GL.FLOAT, false, verticesData.vertexStride * Float32Array.BYTES_PER_ELEMENT, stride* Float32Array.BYTES_PER_ELEMENT);
+				stride += attribute.size;
+				
+				
+			}
 			
-			//trace(attribute);
-			pointer = GL.getAttribLocation(shaderProgram, attribute.name);
-			GL.enableVertexAttribArray(pointer);
-			//GL.enableVertexAttribArray(attribute.index);
-			//GL.vertexAttribPointer(attribute.index, attribute.size, GL.FLOAT, false, renderedData.vertexStride * Float32Array.BYTES_PER_ELEMENT, stride* Float32Array.BYTES_PER_ELEMENT);
-			GL.vertexAttribPointer(pointer, attribute.size, GL.FLOAT, false, verticesData.vertexStride * Float32Array.BYTES_PER_ELEMENT, stride* Float32Array.BYTES_PER_ELEMENT);
-			stride += attribute.size;
-			
-			
+			GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, EBO);
+			GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, indices.byteLength, indices, GL.DYNAMIC_DRAW);
 		}
 		
-		GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, EBO);
-		GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, indices.byteLength, indices, GL.DYNAMIC_DRAW);
 		
 		var camera:Camera;
 		for (cam in cameras)
@@ -221,23 +231,9 @@ class Visual extends AbstractVisual implements IRenderable
 			GL.scissor(camera.viewport.x,BeardGame.Get().window.height - camera.viewport.y - camera.viewport.height, camera.viewport.width, camera.viewport.height);
 			
 			GL.uniformMatrix4fv(GL.getUniformLocation(shaderProgram , "view"), 1, false, camera.view);
-			Light.SetUniforms(shaderProgram);
-			
-			
-							
-			
-			if (indicesPerObject> 0){
-				//trace(verticesData.activeDataCount);
-				//GL.drawElementsInstanced(drawMode, 6, GL.UNSIGNED_SHORT, 0,10);
-				GL.drawElements(drawMode, indicesData.length, GL.UNSIGNED_SHORT, 0);
+			LightManager.Get().SetUniforms(shaderProgram, this.lightGroup);
 					
-			}
-			else
-			{
-				//trace(verticesData.activeDataCount);
-				
-				GL.drawArrays(drawMode, 0, verticesData.activeDataCount*verticesData.vertexPerObject);
-			}
+			GL.drawElements(drawMode, indicesData.length, GL.UNSIGNED_SHORT, 0);
 		
 			drawCount++;
 			
